55e7f3650c6f65b581c3d35e76921779e4bd68d8
Dalton Hughes <dhughes3223@gmail.com>
2023-04-09 16:57:28 -0400

    Initial commit

---
+++ src/test/java/com/example/fishinformation/FishInformationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fishinformation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishInformationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
